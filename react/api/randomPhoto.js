381e96057b2481faf8688c0d2fa1576840258efc
CaptainSquid9 <83174650+CaptainSquid9@users.noreply.github.com>
2024-10-22 18:11:55 -0500

    Error with func

--- react/api/randomPhoto.js
+++ react/api/randomPhoto.js
@@ -52,7 +52,7 @@ export default async function handler(req, res) {
     }
 
     // Fetch the ArrayBuffer data for each file
-    const buffers = await Promise.all(fileIds.map(fetchPhoto()));
+    const buffers = await Promise.all(fileIds.map(fetchPhoto));
 
     res.status(200).json({ images: buffers });
   } catch (error) {
