55be455a8cac0aa5b10aa29cc4934fae531c0e57
CaptainSquid9 <83174650+CaptainSquid9@users.noreply.github.com>
2024-10-23 01:18:34 -0500

    test if blob not found

--- react/api/randomPhoto.js
+++ react/api/randomPhoto.js
@@ -1,3 +1,5 @@
+import { blob } from "stream/consumers";
+
 const { google } = require("googleapis");
 const { PassThrough } = require("stream");
 
@@ -53,7 +55,10 @@ export default async function handler(req, res) {
 
     // Fetch the ArrayBuffer data for each file
     const blobs = await Promise.all(fileIds.map(fetchPhoto));
-
+    if (!blobs || blobs.length === 0) {
+      res.status(404).json({ error: "No blobs found" });
+      return;
+    }
     res.status(200).json({ images: blobs });
   } catch (error) {
     console.error("Error fetching photo:", error);
